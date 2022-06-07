37749007aeffa10b7677d4ff6e6a3362299073f3
dinogajic <92339286+dinogajic@users.noreply.github.com>
2022-06-07 12:32:02 +0200

    Registration

--- src/index.js
+++ src/index.js
@@ -24,14 +24,14 @@ async function run() {
 
       res.json(doc);
   })
+
   
-  app.post("/user", async(req, res) => {
+/*   app.post("/user", async(req, res) => {
     let data = req.body
-
     await client.connect()
-
     let database = client.db('myportfolio'); 
 
+  try {
     await database.collection("user").createIndex({username: 1}, {unique: true})
 
     let register_doc = {
@@ -39,15 +39,47 @@ async function run() {
       password: await bcrypt.hash(data.password, 8)
     }
 
-    try {
     let register = await database.collection("user").insertOne(register_doc)
 
     res.json(register)
   }  catch(e) {    
     if(e.code == 11000) {
-    throw new Error("Korisnik već postoji!")
+    return res.json("Korisnik već postoji!")
   }}
 })
+ */
+
+
+app.post("/register", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  await client.connect()
+  let database = client.db('myportfolio'); 
+  console.log(lastName);
+  console.log(lastName);
+  const pword = await bcrypt.hash(password, 10);
+  console.log(pword);
+  
+  try {
+    await database.collection("user").createIndex({email: 1}, {unique: true})
+
+    const response = await database.collection("user").insertOne({
+      email: email,
+      password: pword,
+      firstName: firstName,
+      lastName: lastName,
+    });
+
+    console.log(response);
+
+    console.log("User created successfully: " + response);
+    res.json("User created successfully: " + response);
+  } catch (error) {
+    if (error.code == 11000) { // it could be .status, .code etc.. not sure
+      return res.json({ status: "error", msg: "User already exist." });
+    }
+    return res.json({ status: "error" });
+  }
+});
 }
 run().catch(console.dir);
 
