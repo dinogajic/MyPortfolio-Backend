b77b0f360b72aa1f290f5dae6245573f12c1d5a6
dinogajic <92339286+dinogajic@users.noreply.github.com>
2022-07-04 16:54:25 +0200

    Save/Read files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-import express from "express";
-import cors from "cors";
-import bcryptjs from "bcryptjs";
-import mongo from "mongodb"
+import express from "express"
+import cors from "cors"
+import bcryptjs from "bcryptjs"
+import mongoose from "mongoose"
 import { MongoClient } from "mongodb"
 import { ObjectId } from "mongodb"
 import jwt from "jsonwebtoken"
 import multer from "multer"
-import { GridFsStorage } from "multer-gridfs-storage";
-/* import  Grid  from "gridfs-stream"; */
+import { GridFsStorage } from "multer-gridfs-storage"
+import Grid from "gridfs-stream"
+import crypto from "crypto"
+import path from "path"
+import methodOverride from 'method-override';
 
 
 
@@ -45,14 +48,37 @@ const fileStorage = multer.diskStorage({
 
 const upload = multer({storage: fileStorage}) */
 
+const mongoURI = "mongodb+srv://myportfolio-wa:webappsprojekt@myportfolio.ieynb.mongodb.net/pic?retryWrites=true&w=majority";
+
+const conn = mongoose.createConnection(mongoURI);
+
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
 const storage = new GridFsStorage({
-  url: "mongodb+srv://myportfolio-wa:webappsprojekt@myportfolio.ieynb.mongodb.net/pic?retryWrites=true&w=majority",
+  url: mongoURI,
   file: (req, file) => {
-    return {
-      filename: 'file_' + Date.now()
-    };
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
   }
 });
+
 const upload = multer({ storage });
 
 
@@ -216,13 +242,27 @@ app.post("/portfolio", [verify], async (req, res) => {
 //TEST
 
 
-app.get("/pic", async(req, res) => {
-  await client.connect()
-  let database = client.db('pic');
-  
-  let doc = await database.collection("fs.chunks").find().toArray()
-  res.json(doc);
-})
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
 
 app.post("/pic", upload.single("image"), async (req, res) => {
   console.log("PIC created successfully");
